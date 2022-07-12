72ca1eea748d64778aec97d1ee103881264e5b2e
Minjae Chang <minjaejchang@gmail.com>
2022-07-12 20:28:24 +1200

    move connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => console.log('Successfully connected!'))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
