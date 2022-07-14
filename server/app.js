72bedb2fdce54f4846683c67e29ebf16de9da9e3
Minjae Chang <minjaejchang@gmail.com>
2022-07-14 20:34:35 +1200

    Add validate

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+
 const postsRouter = require('./router/posts');
 
 const app = express();
@@ -14,7 +15,6 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-  console.error(error);
   res.sendStatus(500);
 });
 
--- server/router/posts.js
+++ server/router/posts.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const postController = require('../controller/posts');
+const { body } = require('express-validator');
+const validate = require('../middleware/validate');
 
 const router = express.Router();
+
+const validatePost = [
+  body('title')
+    .notEmpty()
+    .trim()
+    .withMessage('Enter the title')
+    .isLength({ min: 2, max: 30 })
+    .withMessage('title should be at least 2 characters long'),
+  body('body')
+    .notEmpty()
+    .trim()
+    .withMessage('Please enter something')
+    .isLength({ min: 2 })
+    .withMessage('body should be at least 2 characters long'),
+  validate,
+];
 
 // GET /posts
 router.get('/', postController.getPosts);
@@ -10,10 +28,10 @@ router.get('/', postController.getPosts);
 router.get('/:id', postController.getPost);
 
 // POST /posts
-router.post('/', postController.createPost);
+router.post('/', validatePost, postController.createPost);
 
 // PUT /posts/:id
-router.put('/:id', postController.updatePost);
+router.put('/:id', validatePost, postController.updatePost);
 
 // DELETE /posts/:id
 router.delete('/:id', postController.deletePost);
